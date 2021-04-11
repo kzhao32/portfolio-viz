3d9cf590cecb60028900d48aa126fc54d6b3f9e8
Kai Zhao <kzhao32@cs.wisc.edu>
2021-04-10 20:44:22 -0700

    added call to fetch to grab stock price and price change

--- portfolio-viz.js
+++ portfolio-viz.js
@@ -13,22 +13,26 @@ class Asset {
   }
 }
 
-fReader.onload = function(e) {
+fReader.onload = async function(e) {
   // Parse uploaded file.
   let parsedCsv = Papa.parse(e.target.result);
-  // Check if header row is not present.
-  let startIndex = 0
-  if (typeof(parsedCsv.data[0][1]) === "number") {
-    startIndex = 1;
-  }
-
-  for (let i = startIndex; i < parsedCsv.data.length; i++) {
-
+  for (let i = 0; i < parsedCsv.data.length; i++) {
+    // Account for header and empty rows.
+    if (parsedCsv.data[i].length < 2 || parsedCsv.data[i][1].length == 0 || isNaN(parsedCsv.data[i][1])) {
+      continue;
+    }
     // Get stock price here.
+    let response = await fetch('https://us-central1-stock-price-api.cloudfunctions.net/stock-price-api', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify({ticker: parsedCsv.data[i][0]})
+    });
+    let responseJson = await response.json();
 
     // Update data array.
-    assets.push(new Asset(
-      parsedCsv.data[i][0], parsedCsv.data[i][1], 100, 1));
+    assets.push(new Asset(parsedCsv.data[i][0], parsedCsv.data[i][1], responseJson.price, responseJson.percent_change));
   }
   console.log(assets);
 }
