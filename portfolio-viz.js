32a2b09f57ed4ef7802f17f0157259328fa41910
Kai Zhao <kzhao32@cs.wisc.edu>
2021-04-10 23:34:18 -0700

    updated to make only 1 call to stock-price-api

--- portfolio-viz.js
+++ portfolio-viz.js
@@ -16,23 +16,34 @@ class Asset {
 fReader.onload = async function(e) {
   // Parse uploaded file.
   let parsedCsv = Papa.parse(e.target.result);
+  stocks_price_check = []
   for (let i = 0; i < parsedCsv.data.length; i++) {
     // Account for header and empty rows.
     if (parsedCsv.data[i].length < 2 || parsedCsv.data[i][1].length == 0 || isNaN(parsedCsv.data[i][1])) {
       continue;
     }
-    // Get stock price here.
-    let response = await fetch('https://us-central1-stock-price-api.cloudfunctions.net/stock-price-api', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-      },
-      body: JSON.stringify({ticker: parsedCsv.data[i][0]})
-    });
-    let responseJson = await response.json();
+    stocks_price_check.push(parsedCsv.data[i][0])
+  }
+  // Get stock prices here.
+  let response = await fetch('https://us-central1-stock-price-api.cloudfunctions.net/stock-price-api', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify({tickers: stocks_price_check})
+  });
+  let responseJson = await response.json();
 
+  // Assume that responses come back in the same order that stocks_price_check requested.
+  // Then need to filter out header and empty rows again to get the index to match with the responses.
+  for (let i = 0, response_index = 0; i < parsedCsv.data.length; i++) {
+    // Account for header and empty rows.
+    if (parsedCsv.data[i].length < 2 || parsedCsv.data[i][1].length == 0 || isNaN(parsedCsv.data[i][1])) {
+      continue;
+    }
     // Update data array.
-    assets.push(new Asset(parsedCsv.data[i][0], parsedCsv.data[i][1], responseJson.price, responseJson.percent_change));
+    assets.push(new Asset(responseJson[response_index].ticker, parsedCsv.data[i][1], responseJson[response_index].price, responseJson[response_index].percent_change));
+    response_index++;
   }
   console.log(assets);
 }
